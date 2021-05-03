bc4df6ae3409be0737ff20e7a06af549bd401659
김도영 <95doyoung@gmail.com>
2021-05-03 22:59:18 +0900

    first commit

---
+++ src/main/java/me/doyoung/study/apifirstdesign/ApifirstdesignApplication.java
@@ -0,0 +1,13 @@
+package me.doyoung.study.apifirstdesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApifirstdesignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApifirstdesignApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/doyoung/study/apifirstdesign/ApifirstdesignApplicationTests.java
@@ -0,0 +1,13 @@
+package me.doyoung.study.apifirstdesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApifirstdesignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

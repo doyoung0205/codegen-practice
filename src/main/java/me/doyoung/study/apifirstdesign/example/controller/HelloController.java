e1fafb129762f2ee327b048fb2bd13c6f0a472f1
김도영 <95doyoung@gmail.com>
2021-05-06 20:13:54 +0900

    Franchise code gen copy

---
+++ src/main/java/me/doyoung/study/apifirstdesign/example/controller/HelloController.java
@@ -0,0 +1,13 @@
+package me.doyoung.study.apifirstdesign.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping()
+    public String hello() {
+        return "hello";
+    }
+}
